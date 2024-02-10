fe3ad8f5e46960ac071286627f030e18d1663422
garand277 <145905657+garand277@users.noreply.github.com>
2024-02-10 14:07:52 +0500

    Update 06_songs_list.py

--- 06_songs_list.py
+++ 06_songs_list.py
@@ -24,6 +24,8 @@ violator_songs_list = [
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
 # TODO здесь ваш код
+tot_time = round(violator_songs_list[3][1] + violator_songs_list[5][1] + violator_songs_list[8][1], 2)
+print('Три песни звучат', tot_time, 'минут')
 
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
@@ -42,3 +44,5 @@ violator_songs_dict = {
 #   А другие три песни звучат ХХХ минут
 
 # TODO здесь ваш код
+tot_time2 = round(violator_songs_dict['Sweetest Perfection'] + violator_songs_dict['Policy of Truth'] + violator_songs_dict['Blue Dress'], 2)
+print('А другие три песни звучат', tot_time2, 'минут')
