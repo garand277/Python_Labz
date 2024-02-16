3cdd80ee0442d53c2e6729c7ade4b5d6e984868b
garand277 <145905657+garand277@users.noreply.github.com>
2024-02-16 21:01:45 +0500

    Update 00_distance.py

--- 00_distance.py
+++ 00_distance.py
@@ -16,8 +16,11 @@ distances = {}
 
 # TODO здесь заполнение словаря
 
+for city1, cord1 in sites.items():
+    distances[city1] = {}
+    for city2, cord2 in sites.items():
+        if city1 != city2:
+            x = round(((cord1[0] - cord2[0]) ** 2 + (cord1[1] - cord2[1]) ** 2) ** 0.5, 2)
+            distances[city1][city2] = x
+
 print(distances)
-
-
-
-
